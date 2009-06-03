2d5d82285b856b4253abb98abb6306aedd61c710
dread <devnull@localhost>
2009-06-03 13:16:18 +0100

    [analysis]: Ticket 24 - added US jurisdiction to PD tester.

--- pd.py
+++ pd.py
@@ -55,7 +55,9 @@ def float_date(year, month=0, day=0):
 def determine_status(work, jurisdiction):
     # now dispatch on jurisdiction (+ work type?)
     # note two letter country codes based on ISO 3166
-    if jurisdiction == 'ca':
+    if jurisdiction == 'us':
+        pd_calculator = CalculatorUnitedStates
+    elif jurisdiction == 'ca':
         pd_calculator = CalculatorCanada
     elif jurisdiction in ('uk', 'gb') :
         pd_calculator = CalculatorUk
@@ -94,6 +96,9 @@ class CalcParcel(object):
         return "date_pd=%s pd_prob=%s log=%s" % (self.date_pd, self.pd_prob, self.log)
 
 class CalculatorBase(object):
+    """A Public Domain Calculator
+    when=None means today's date
+    """
     def __init__(self, when):
         self.author_list = None
         if when:
@@ -197,11 +202,62 @@ class CalculatorUk(CalculatorBase):
         expiry = float(int(parcel.most_recent_death_date + 1 + 70))
         parcel.log.append('PD expires at "death + 1st Jan + 70" (%s + 70 = %s)' % (parcel.most_recent_death_date, expiry))
         return pd_expires(expiry, parcel)
+
+class CalculatorUnitedStates(CalculatorBase):
+    def __init__(self, when=None):
+        CalculatorBase.__init__(self, when)
+
+    def get_work_status(self, parcel):
+        assert type(parcel) == CalcParcel
+        
+        work = parcel.work
+        
+        # Call this when we know when pd expires
+        def pd_expires(expiry_date, parcel):
+            if expiry_date > self._now:
+                parcel.pd_prob = 0.0
+            else:
+                parcel.pd_prob = 1.0
+            return parcel        
+        
+        parcel.log.append("We're assuming it was published.")
+        parcel.log.append("We're assuming it was published in the US.")
+        parcel.log.append("We're assuming it was published with a valid copyright notice.")
+
+        # Different rules depending on when it was published
+        pubyear = work.date_ordered
+        if pubyear < 1923:
+            # PUBLICATION + 28+28 years
+            parcel.log.append('Published before 1923 (actually %s)' % pubyear)
+            expiry = float(int(pubyear + 28 + 28))
+            parcel.log.append('PD expires at "publication + 28 + 28" (%s + 28 + 28 = %s)' % (pubyear, expiry))
+            return pd_expires(expiry, parcel)
+        elif pubyear < 1964:
+            # PUBLICATION + 95 years
+            parcel.log.append('Published between 1923 and 1964 (actually %s)' % pubyear)
+            parcel.log.append('Assuming its copyright was renewed')
+            expiry = float(int(pubyear + 95))
+            parcel.log.append('PD expires at "publication + 95" (%s + 95 = %s)' % (pubyear, expiry))
+            return pd_expires(expiry, parcel)
+        elif pubyear < 1978:
+            # PUBLICATION + 95 years
+            parcel.log.append('Published between 1964 and 1978 (actually %s)' % pubyear)
+            expiry = float(int(pubyear + 95))
+            parcel.log.append('PD expires at "publication + 95" (%s + 95 = %s)' % (pubyear, expiry))
+            return pd_expires(expiry, parcel)
+            pdyear = pubyear + 95
+
+        # DEATH + 70 years
+        parcel.log.append('Published after 1978 (actually %s)' % pubyear)
+        parcel.log.append('Assuming it was not published by a corporation or under a pseudonym.')
+        self.calc_death_dates(parcel)
+        expiry = float(int(parcel.most_recent_death_date + 70))
+        parcel.log.append('PD expires at "death + 70" (%s + 70 = %s)' % (parcel.most_recent_death_date, expiry))
+        return pd_expires(expiry, parcel)
+        
+
             
 class CalculatorCanada(CalculatorBase):
-    """A Public Domain Calculator
-    when=None means today's date
-    """
     def __init__(self, when=None):
         CalculatorBase.__init__(self, when)
 
--- tests/test_pd.py
+++ tests/test_pd.py
@@ -2,17 +2,16 @@ import pdw.pd as pd
 from datetime import datetime
 
 class TestPdStatusCanada:
-    def __init__(self):
-        self.jurisdiction = 'ca'
+    jurisdiction = 'ca'
 
-    @classmethod
-    def setup_class(self):
+    def setUp(self):
         class X:
             pass
 
         artist = X()
         artist.name = 'Bloggs, Joe'
         artist.death_date_ordered = 1900.0
+        artist.birth_date_ordered = None
 
         work = X()
         work.title = 'Song for the Common Man'
@@ -83,20 +82,21 @@ class TestPdStatusCanada:
         assert pdstatus.pd_prob == 0.0
 
 class TestPdStatusUk:
-    def __init__(self):
-        self.jurisdiction = 'uk'
+    jurisdiction = 'uk'
 
     @classmethod
-    def setup_class(self):
+    def setUp(self):
         class X:
             pass
 
         composer = X()
         composer.name = 'Schumann, Robert'
+        composer.birth_date_ordered = 1830.0
         composer.death_date_ordered = 1900.0
 
         singer = X()
         singer.name = 'Terfel, Bryn'
+        singer.birth_date_ordered = 1974
         singer.death_date_ordered = None
 
         composition_work = X()
@@ -129,6 +129,7 @@ class TestPdStatusUk:
         self.cd_item = cd_item
         self.score_item = score_item
 
+
     def _log_has(self, str, log):
         for log_line in log:
             if str in log_line:
@@ -152,6 +153,7 @@ class TestPdStatusUk:
         assert pdstatus.pd_prob == 0.0
 
     def test_work_status_3(self):
+        print "NAME", self.composition_work.persons[0].name
         self.composition_work.persons[0].name = 'anonymous'
         self.composition_work.date_ordered = 1930.0
 
@@ -174,6 +176,125 @@ class TestPdStatusUk:
         pdstatus = pd.determine_status(self.recording_work, self.jurisdiction)
         print pdstatus
         assert self._log_has('first publication + 1st Jan + 50', pdstatus.log)
+        assert pdstatus.pd_prob == 0.0
+
+    def test_work_status_6(self):
+        self.composer.birth_date_ordered = 1820.0
+        self.composer.death_date_ordered = None
+
+        pdstatus = pd.determine_status(self.composition_work, self.jurisdiction)
+        print pdstatus
+        assert self._log_has('assuming died 100 years after birth', pdstatus.log)
+        assert self._log_has('death + 1st Jan + 70', pdstatus.log)
+        assert pdstatus.pd_prob == 1.0
+
+    def test_work_status_7(self):
+        self.composer.birth_date_ordered = 1920.0
+        self.composer.death_date_ordered = None
+
+        pdstatus = pd.determine_status(self.composition_work, self.jurisdiction)
+        print pdstatus
+        assert self._log_has('assuming died 100 years after birth', pdstatus.log)
+        assert self._log_has('death + 1st Jan + 70', pdstatus.log)
+        assert pdstatus.pd_prob == 0.0
+
+class TestPdStatusUnitedStates:
+    jurisdiction = 'us'
+
+    @classmethod
+    def setUp(self):
+        class X:
+            pass
+
+        composer = X()
+        composer.name = 'Schumann, Robert'
+        composer.birth_date_ordered = 1830.0
+        composer.death_date_ordered = 1900.0
+
+        singer = X()
+        singer.name = 'Terfel, Bryn'
+        singer.birth_date_ordered = 1974
+        singer.death_date_ordered = None
+
+        composition_work = X()
+        composition_work.title = 'I love flowers'
+        composition_work.persons = [ composer ]
+        composition_work.date_ordered = 1945.0
+        composition_work.type = 'composition'
+
+        recording_work = X()
+        recording_work.title = 'Bryn Terfel sings "I love flowers"'
+        recording_work.persons = [ composer, singer ]
+        recording_work.date_ordered = 1945.0
+        recording_work.type = 'recording'
+
+        cd_item = X()
+        cd_item.title = 'Songs CD'
+        cd_item.date_ordered = 1985.0
+        cd_item.works = [composition_work, recording_work]
+
+        score_item = X()
+        score_item.title = 'Song score'
+        score_item.date_ordered = 1955.0
+        score_item.works = [composition_work]
+
+        composition_work.items = [cd_item, score_item]
+
+        self.composer = composer
+        self.composition_work = composition_work
+        self.recording_work = recording_work
+        self.cd_item = cd_item
+        self.score_item = score_item
+
+
+    def _log_has(self, str, log):
+        for log_line in log:
+            if str in log_line:
+                return True
+        return False
+        
+    def test_work_status_1(self):
+        self.composition_work.date_ordered = 1920.0
+
+        pdstatus = pd.determine_status(self.composition_work, self.jurisdiction)
+        print pdstatus
+        assert self._log_has('publication + 28 + 28', pdstatus.log)
+        assert pdstatus.pd_prob == 1.0
+
+    def test_work_status_2(self):
+        self.composition_work.date_ordered = 1950.0
+
+        pdstatus = pd.determine_status(self.composition_work, self.jurisdiction)
+        print pdstatus
+        assert self._log_has('publication + 95', pdstatus.log)
+        assert self._log_has('Assuming its copyright was renewed', pdstatus.log)
+        assert pdstatus.pd_prob == 0.0
+
+    def test_work_status_3(self):
+        self.composition_work.date_ordered = 1970.0
+
+        pdstatus = pd.determine_status(self.composition_work, self.jurisdiction)
+        print pdstatus
+        assert self._log_has('publication + 95', pdstatus.log)
+        assert not self._log_has('Assuming its copyright was renewed', pdstatus.log)
+        assert pdstatus.pd_prob == 0.0
+
+    def test_work_status_4(self):
+        self.composition_work.date_ordered = 1980.0
+        self.composer.death_date_ordered = 1900
+
+        pdstatus = pd.determine_status(self.composition_work, self.jurisdiction)
+        print pdstatus
+        assert self._log_has('death + 70', pdstatus.log)
+        assert pdstatus.pd_prob == 1.0
+
+    def test_work_status_5(self):
+        self.composition_work.date_ordered = 1980.0
+        self.composer.death_date_ordered = 1990
+
+        pdstatus = pd.determine_status(self.composition_work, self.jurisdiction)
+        print pdstatus
+        assert self._log_has('death + 70', pdstatus.log)
         assert pdstatus.pd_prob == 0.0
 
 
