e9075ae05a8453d4c8081fc5c877147b9344ad75
rgrp <devnull@localhost>
2009-11-03 15:16:36 +0000

    [misc][m]: (old: 1w+) various fairly minor changes in anlysis and pd code.
      * pdw/analyze.py: cumulative stats, show pd under 14+14 etc
      * pdw/pd.py: new fast_pd_year method.
      * pdw/getdata/marc.py: minor changes to make PdwParser functional.

--- pd.py
+++ pd.py
@@ -420,6 +420,12 @@ def fast_pd_year(extras_key=FAST_PD_YEAR_KEY):
         if death_date: pd_year = death_date + 70
         elif birth_date: pd_year = birth_date + OLDEST_PERSON + 70
         else: pd_year = date_of_publication + OLDEST_PERSON - LEAST_AGE_AT_PUBLICATION + 70
+
+    # would be nice to use sql directly but can't generate primar_key id
+    insert into extra select item.id, person.death_date_ordered from
+    item join item_2_person on item.id = item_2_person.item_id join person on
+    item_2_person.person_id = person.id where person.death_date_ordered is not
+    null;
     '''
     import pdw.model as model
     import pdw.search
@@ -432,7 +438,12 @@ def fast_pd_year(extras_key=FAST_PD_YEAR_KEY):
     # Rather than use insert into use select then insert
     ptab = model.person_table
     itab = model.item_table
-    q = sql.select([itab.c.id, sql.func.coalesce(ptab.c.death_date_ordered,
+    q = sql.select([itab.c.id, ptab.c.death_date_ordered],
+        from_obj=pdw.search.QueryHelper.item_person()
+        )
+    q = q.where(ptab.c.death_date_ordered!=None)
+    # more complex algorithm
+    q_cmplx = sql.select([itab.c.id, sql.func.coalesce(ptab.c.death_date_ordered,
         ptab.c.birth_date_ordered+OLDEST_PERSON,
         itab.c.date_ordered + (OLDEST_PERSON - least_age_at_publication)) +
         post_death_term
@@ -453,6 +464,10 @@ def fast_pd_year(extras_key=FAST_PD_YEAR_KEY):
     cmd = model.extra_table.delete(model.extra_table.c.key==extras_key)
     out = cmd.execute()
     logger.info('Deleting all existing entries with key: %s' % extras_key)
+
+    # before we begin may need to update extra_id_seq
+    # maxval = select max(id) + 1 from extra;
+    # select setval('extra_id_seq', maxval);
 
     inserts = get_inserts(q.execute().fetchall())
     if inserts:
