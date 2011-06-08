5431c5c809ff2487de0a9fa0b5593eecc6150e8d
rgrp <http://rufuspollock.org>
2011-06-08 19:08:53 +0100

    [pdcalc][s]: get_pd_status convenience method (plus tests).

--- pdcalc/test/test_with_data.py
+++ pdcalc/test/test_with_data.py
@@ -1,3 +1,4 @@
+import pdcalc
 import pdcalc.uk as uk
 import pdcalc.fr as fr
 from pdcalc.work import Work
@@ -6,7 +7,6 @@ from pdcalc.bibliographica import *
 
 from pprint import pprint
 
-# Run this test: nosetests pdcalc/CalculatorFR.py
 def test():
     calc = {}
     results = {} 
@@ -14,26 +14,16 @@ def test():
     for k in calculators.keys():
         calc[k] = Calculator(k)
    
-
- 
-    #for f in [ '%02d' % i for i in range(2)]:
     data = load_from_file("pdcalc/test/data/01.json")
     results["uk"] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     results["fr"] = [0, 0, 0, 0, 1, 1, 1, 0, 1, 0]
 
-    n = 0;
-    for i in data:
+    for n,i in enumerate(data):
         test = Bibliographica(i)
         work = Work(test.data)
 
-	#pprint(test.data)
-
         for k, v in calc.items():
-            #print "%s in [%s] = %s\n" % (work.title, k, v.get_status(work))
-	    #if v.get_status(work) != results[k][n]: raise ValueError("error")
             assert v.get_status(work) == results[k][n]
-        n+=1
-
-#test()
-
+            assert pdcalc.get_pd_status(work, k)[k]['pd'] == results[k][n]
+            assert pdcalc.get_pd_status(work)[k]['pd'] == results[k][n]
                 
