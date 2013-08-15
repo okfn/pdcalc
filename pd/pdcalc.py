ffe9ec300ef2321dbf4179492d1225c058e39510
Marco Montanari <sirmmo@gmail.com>
2013-08-15 15:40:23 +0200

    pdcalc update

--- pd/pdcalc.py
+++ pd/pdcalc.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
   parser.add_argument('-c', '--country', dest='country',  help='country for which to test', required=True)
   parser.add_argument('-i', '--instance', dest='instance',  help='instance to test', required=True)
   parser.add_argument('-g', '--global', dest='global',  help='global mapping', default="global.rdf")
-  parser.add_argument('-m', '--mode', dest='mode',  help='instance definition mode', choices=['file', 'url', 'filelist', 'urllist'], default="file")
+  parser.add_argument('-l', '--list', dest="list", help="True if list", action='store_true', default=False)
+  parser.add_argument('-m', '--mode', dest='mode',  help='instance definition mode', choices=['file', 'url'], default="file")
   parser.add_argument('-f', '--format', dest='format',  help='format of the instances', choices=['rdf', 'json'], default="rdf")
   args = parser.parse_args()
 
