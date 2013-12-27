d247ba16c8a7c5e612e02cd8e300438f7af8e384
Marco Montanari <sirmmo@gmail.com>
2013-12-27 20:04:41 +0100

    major updates

--- pd/france/bnf/n46.py
+++ pd/france/bnf/n46.py
@@ -5,7 +5,7 @@ import json
 def evaluate_question(model, *args, **kwargs):
 	author = "http://data.bnf.fr/ark:/12148/cb13338797s#foaf:Person"
 	#Query to get creator uri
-	ids = json.load(open('france/morts.json', 'r'))
+	ids = json.load(open('france/bnf/morts.json', 'r'))
 
 	author_id = author.split('#')[0].split('/')[5][2:-1]
 
