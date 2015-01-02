0abe739c9ed948ebce5ba31f27485560d849ec2c
Marco Montanari <sirmmo@gmail.com>
2015-01-02 12:29:01 +0100

    date

--- pd/flavours/bnf/n49.py
+++ pd/flavours/bnf/n49.py
@@ -13,7 +13,8 @@ def evaluate_question(model, *args, **kwargs):
 	prefix xsd: <http://www.w3.org/2001/XMLSchema#> 
 	prefix bio: <http://vocab.org/bio/0.1/>
 	SELECT ?date
-	WHERE { ?a dc:creator ?bio. ?bio bio:Death ?date } 
+	WHERE { ?a dc:creator ?bio. 
+			?bio bio:death ?date } 
 	"""
 
 	result = model.query(q)
